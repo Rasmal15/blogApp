52607874d39b0d59ef1f7b2214652c002b6c1cc7
rasmal15 <rasmalnp123@gmail.com>
2025-01-23 10:23:17 +0530

    added gitignore

--- homeApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.homeFunction,name='home'),
-    path('view_profile', views.viewProfileFunction,name='view_profile')
-]
